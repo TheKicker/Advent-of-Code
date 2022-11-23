549a4e82df785bb37549b8c7b4adeb99460eff29
The Kicker <cav.lemasters@geneva.edu>
2022-11-23 07:02:58 -0500

    Day 2 Part 2 finished

--- 2020/day-2/part-two.js
+++ 2020/day-2/part-two.js
@@ -18,4 +18,43 @@ for (var line = 0; line < file.length; line++)
     const pass = match[4];
 
     // do stuff
+    var pos = []
+
+    for(var i = 0; i < pass.length; i++)
+    {
+        if(pass[i] === char)
+        {
+            // Had to add +1 because these dorks have no concept of index 0
+            pos.push(i + 1)
+        }
+    }
+
+    // Debug
+    // console.log("--------------")
+    // console.log(min)
+    // console.log(max)
+    // console.log(char)
+    // console.log(pass)
+    // console.log(pos)
+
+    if(pos.includes(min))
+    {
+        if(pos.includes(max))
+        {
+            // console.log("invalid")
+            invalid++;
+        } else {
+            // console.log("valid")
+            valid++;
+        }
+    } else if (pos.includes(max))
+    {
+        // console.log("valid")
+        valid++;
+    } else {
+        // console.log("invalid")
+        invalid++;
+    }
+
+    console.log(`Valid: ${valid} / Invalid: ${invalid} / ${valid/file.length}%`)
 }
