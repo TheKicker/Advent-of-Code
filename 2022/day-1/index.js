5c5075ac80fdf7a2af0c9f9753ea58486aaa7b03
The Kicker <cav.lemasters@geneva.edu>
2022-12-01 10:21:30 -0500

    Day One complete :sparkles:

---
+++ 2022/day-1/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+var arr = fs.readFileSync('data.txt').toString().split("\n");
+
+// iterate through array
+// if it contains a number
+// add to current calories
+// set highest calorie count and location
+// if it contains a space
+// go to next elf
+
+var currentCalories = 0;
+var maxArr = [];
+
+for(var i = 0; i < arr.length; i++)
+{
+    if(arr[i] != '')
+    {
+        currentCalories = currentCalories + parseInt(arr[i]);
+
+    } else {
+        maxArr.push(currentCalories)
+        currentCalories = 0;
+    }
+}
+maxArr = maxArr.sort((a,b)=>b-a)
+console.log(`Highest: ${maxArr[0]} / Top-3 Sum: ${maxArr[0]+maxArr[1]+maxArr[2]}`)
