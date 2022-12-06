db83e10df7166ff4322f5848534f21e7dfb6505c
The Kicker <cav.lemasters@geneva.edu>
2022-12-06 15:30:57 -0500

    Day 6 started

--- 2022/day-6/index.js
+++ 2022/day-6/index.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
-let data = fs.readFileSync('./day-6/data.txt', 'utf-8').toString().split("\n");
+let data = fs.readFileSync('./test-data.txt', 'utf-8').toString();
 
+for(let i = 4; i < data.length; i++)
+{
+    checkRelatives(i)
+}
+
+function checkRelatives(currentIndex)
+{   
+    let minusThree = currentIndex - 4
+    console.log(data.slice(minusThree, currentIndex))
+}
