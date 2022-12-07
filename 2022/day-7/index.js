399dc31ad938e3975f85c1250fb2cd27824f65ee
The Kicker <cav.lemasters@geneva.edu>
2022-12-07 10:32:28 -0500

    Day 7 is going to send me on my joker character arc...

---
+++ 2022/day-7/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let data = fs.readFileSync('./day-6/data.txt', 'utf-8').toString();
+
+for(let i = 0; i < data.length; i++)
+{
+    // Do stuff
+    // Day 7 is going to send me on my joker character arc...
+    // Where do I even begin?
+}
